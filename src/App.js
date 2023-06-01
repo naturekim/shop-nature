83c963091e7378924f122221a83ffa80b88fb70e
jykim <jykim.developer@gmail.com>
2023-06-01 21:01:05 +0900

    Feat: 메인페이지 리스트 구현완료 및 상세페이지 구현중
    - 메인페이지 리스트 조회 및 라우팅 구현
    - 상세페이지 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -11,144 +11,230 @@ import {
   faChevronUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
+import data from "./data.js";
+import { useState } from "react";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
+  const [items, setItems] = useState(data);
+  let navigate = useNavigate();
+  // console.log(items[0]);
+  // console.log(items);
+
   return (
-    <>
-      <div class="wrapper">
-        {/* Header */}
-        <header id="header">
-          <nav class="navbar">
-            <div class="navbar__logo">
-              <a href="#">
-                <h1>
-                  <FontAwesomeIcon icon={faTree} /> 자연산약초세상
-                </h1>
-              </a>
-              {/* 토글버튼 */}
-              <ul class="navbar__toggle">
-                <li>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faCartShopping} />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
+    <div className="wrapper">
+      {/* Header */}
+      <header>
+        <a href="#header" className="topBtn accent">
+          <FontAwesomeIcon icon={faChevronUp} />
+        </a>
+        <nav className="navbar">
+          <div className="navbar__logo">
+            <Link to="/">
+              <h1>
+                <FontAwesomeIcon icon={faTree} /> 자연산약초세상
+              </h1>
+            </Link>
+            {/* 토글버튼 */}
+            <ul className="navbar__toggle">
+              <li>
+                <Link to="/basket">
+                  <FontAwesomeIcon icon={faCartShopping} />
+                </Link>
+              </li>
+              <li>
+                <Link to="/mypage">
+                  <>
                     <FontAwesomeIcon icon={faUser} />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faBars} />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="navbar__search">
-              <input type="text" placeholder="상품검색"></input>
-              <button>
-                <FontAwesomeIcon icon={faX} />
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </button>
-            </div>
-            <ul class="navbar__menu">
+                  </>
+                </Link>
+              </li>
               <li>
                 <a href="#">
+                  <FontAwesomeIcon icon={faBars} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar__search">
+            <input type="text" placeholder="상품검색"></input>
+            <button>
+              <FontAwesomeIcon icon={faX} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+          <ul className="navbar__menu">
+            <li>
+              <Link to="/basket">
+                <>
                   <FontAwesomeIcon icon={faCartShopping} />
                   <span>장바구니</span>
-                </a>
-              </li>
-              <li>
-                <span>
-                  <a href="#">
-                    <FontAwesomeIcon icon={faTruckFast} /> 주문조회
-                  </a>
-                </span>
-              </li>
-              <li>
-                <a href="#">
+                </>
+              </Link>
+            </li>
+            <li>
+              <Link to="/mypage/orderlist">
+                <FontAwesomeIcon icon={faTruckFast} /> <span>주문조회</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="login">
+                <>
                   <FontAwesomeIcon icon={faUser} />
                   <span>로그인</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <nav class="navbar2">
-            <ul class="navbar2__menu">
-              <li>봄 심기 구근</li>
-              <li>여름 심기 구근</li>
-              <li>가을 심기 구근</li>
-              <li>숙근류 (뿌리)</li>
-              <li>묘목 (나무)</li>
-              <li>종자 (씨앗)</li>
-              <li>포트상품</li>
-              <li>자재</li>
-            </ul>
-          </nav>
-        </header>
-        {/* Main */}
-        <main>
-          <section>
-            <div class="section__title">
-              <span>
-                <FontAwesomeIcon icon={faTree} />
-              </span>
-              <h3>이번주 인기상품</h3>
-            </div>
-            <div class="card-list">
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </div>
-          </section>
-        </main>
-        {/* Footer */}
-        <footer class="footer">
-          <ul>
-            <li>
-              <a>사업자정보</a>
-            </li>
-            <li>
-              <a>개인정보처리방침</a>
-            </li>
-            <li>
-              <a>이용약관</a>
+                </>
+              </Link>
             </li>
           </ul>
-          <p>© 2023 자연산약초세상</p>
-        </footer>
-      </div>
-      <a href="#header" class="anchor">
-        <FontAwesomeIcon icon={faChevronUp} />
-      </a>
-    </>
+        </nav>
+        <nav>
+          <ul className="category">
+            <li>봄 심기 구근</li>
+            <li>여름 심기 구근</li>
+            <li>가을 심기 구근</li>
+            <li>숙근류 (뿌리)</li>
+            <li>묘목 (나무)</li>
+            <li>종자 (씨앗)</li>
+            <li>포트상품</li>
+            <li>자재</li>
+          </ul>
+        </nav>
+      </header>
+      {/* Main */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <section>
+                <div className="section__title">
+                  <span>
+                    <FontAwesomeIcon icon={faTree} />
+                  </span>
+                  <h3>이번주 인기상품</h3>
+                </div>
+                <div className="card-list">
+                  {items.length > 0 &&
+                    items.map((item, i) => {
+                      return <Card item={item} key={i} />;
+                    })}
+                </div>
+              </section>
+            </main>
+          }
+        />
+        <Route path="/detail/:id" element={<Detail items={items} />} />
+        <Route path="/basket" element={<div>장바구니페이지</div>} />
+        <Route path="/login" element={<div>로그인페이지</div>} />
+        <Route path="/mypage" element={<Mypage />}>
+          <Route path="orderlist" element={<div>주문조회</div>} />
+          <Route path="myinfo" element={<div>회원정보수정</div>} />
+          <Route path="qnailist" element={<div>문의내역</div>} />
+        </Route>
+        <Route path="*" element={<div>없는페이지</div>} />
+      </Routes>
+
+      {/* Footer */}
+      <footer>
+        <ul>
+          <li>
+            <a>사업자정보</a>
+          </li>
+          <li>
+            <a>개인정보처리방침</a>
+          </li>
+          <li>
+            <a>이용약관</a>
+          </li>
+        </ul>
+        <p>© 2023 자연산약초세상</p>
+      </footer>
+    </div>
   );
 }
 
-function Card(props) {
+function Detail(props) {
+  let [탭, 탭변경] = useState(0);
+  let { id } = useParams();
+  let 찾은상품 = props.items.find((x) => x.id == id);
   return (
-    <div class="card-item">
-      <a href="#">
-        <div class="card-image">
-          <img src="https://cdn.pixabay.com/photo/2019/05/06/16/48/hosta-4183587_960_720.jpg" />
+    <main>
+      <section className="detail">
+        <div className="detail-image">
+          <img src={`${process.env.PUBLIC_URL}/img/${찾은상품.img}.jpg`} />
         </div>
-        <div class="card-desc">
-          <h5>호스타 숙근</h5>
+        <div className="detail-desc">
+          <div>{찾은상품.title}</div>
+          <div>{찾은상품.content}</div>
+          <div>{찾은상품.price}원</div>
           <div>
-            <span>20000원</span>
-            <span>10000원</span>
+            <button className="btn outline-accent">장바구니담기</button>
+            <button className="btn accent">주문하기</button>
           </div>
-          <p>
-            넓은 잎사귀로 노지월동이 가능하여 잎이 아름다워 관상가치가 높은
-            식물입니다
-          </p>
         </div>
-      </a>
-    </div>
-  )
+      </section>
+      <section>
+      <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav.Item>
+      <Nav.Link eventKey="link0" onClick={()=>{탭변경(0)}}>상품설명</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+      <Nav.Link eventKey="link1" onClick={()=>{탭변경(1)}}>상품리뷰</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+      <Nav.Link eventKey="link2" onClick={()=>{탭변경(2)}}>상품문의</Nav.Link>
+      </Nav.Item>
+    </Nav>
+      <TabContent 탭={탭} />
+      </section>
+    </main>
+  );
 }
 
+function TabContent({탭}){
+  return [ <div>상품설명</div>, <div>상품리뷰</div>, <div>상품문의</div> ][탭]
+}
+
+function Card(props) {
+  console.log(props.item);
+  return (
+    <div className="card-item">
+      <Link to={"/detail/" + props.item.id}>
+        <div className="card-image">
+          <img
+            src={process.env.PUBLIC_URL + "/img/" + props.item.img + ".jpg"}
+          />
+        </div>
+        <div className="card-desc">
+          <h5>{props.item.title}</h5>
+          <div>
+            <span>20000원</span>
+            <span>{props.item.price} 원</span>
+          </div>
+          <p>{props.item.content}</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+function Mypage() {
+  return (
+    <section>
+      <h4>마이페이지</h4>
+      <Outlet />
+    </section>
+  );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+let data = [
+    {
+      id : 0,
+      img : "0590010006652",
+      title : "백합-파차노(LO) : 1구",
+      content : "학명 : Lilium hyb. 'paciano'",
+      price : 2000
+    },
+  
+    {
+      id : 1,
+      img : "0590010005822",
+      title : "[구근]글라디올러스-밀카 : 1구",
+      content : "학명 : Gladiolus",
+      price : 2000
+    },
+    
+    {
+      id : 2,
+      img : "0590010006622",
+      title : "[구근]글라디올러스-루트 드 솔레이 : 1구",
+      content : "학명 : Gladiolus",
+      price : 1000
+    },
+    
+    {
+      id : 3,
+      img : "0580010041782",
+      title : "[모종]유칼립투스-실버 달러 : 8cm분",
+      content : "학명 : Eucalyptus gunnii \"Silver dollar\"",
+      price : 2800
+    }
+  ] 
+
+  export default data;
